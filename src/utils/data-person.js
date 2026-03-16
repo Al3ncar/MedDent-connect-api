615784cd8c6964745b9fb322ff30941dab486b94
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-03-15 22:49:03 -0300

    feat: added id data person array

--- src/utils/data-person.js
+++ src/utils/data-person.js
@@ -1,14 +1,16 @@
  const dataAllPerson = [
   {
+    id: 1,
     name: "Joana",
     email: "joana@gmail.com",
     cellPhone: "(11) 98765-4321",
     msg: "exemplo de mensagem",
   },
   {
+    id: 2,
     name: "Amanda",
     email: "amanda@gmail.com",
-    cellPhone: "(11) 12345-6789",
+    cellPhone: "(11) 92456-9869",
     msg: "exemplo de mensagem 2",
   },
 ];
