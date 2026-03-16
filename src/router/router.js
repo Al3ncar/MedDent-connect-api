3f32eda95e7558fd5c581600108152093d8ff323
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-03-15 22:48:41 -0300

    feat: added new router updated and delete

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,8 @@ import {
   getHome,
   getRegisters,
   createRegister,
+  updateRegister,
+  deleteRegister,
 } from "../controllers/controllers.js";
 
 const route = express.Router();
@@ -11,4 +13,7 @@ const route = express.Router();
 route.get("/", getHome);
 route.get("/api", getRegisters);
 route.post("/api/data", createRegister);
+route.put("/api/data/:id", updateRegister);
+route.delete("/api/data/:id", deleteRegister);
+
 export default route;
